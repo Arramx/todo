4e3ff7b0d0939643bf1a0f94e3d2eb629fc4f46e
Mieszko-I <macmik123@gmail.com>
2019-06-17 11:18:15 +0200

    Vue frontend is back

---
+++ client/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div id="app">
+    <Header />
+    <router-view />
+</div>
+</template>
+
+<script>
+import Header from './components/Header';
+
+export default {
+    name: 'app',
+    components: {
+        Header
+    }
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
+body {
+    margin: 0;
+    width: 100vw;
+    height: 100vh;
+    line-height: 0;
+}
+
+* {
+    font-size: 20px;
+    font-family: 'Roboto', sans-serif;
+    box-sizing: border-box;
+}
+</style>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="header">
+    <h1>TodoList</h1>
+    <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+@mixin center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+.header {
+    text-align: center;
+    color: white;
+    background: #222;
+    height: 100px;
+    @include center;
+
+    h1 {
+        font-size: 40px;
+        padding-bottom: 10px;
+    }
+
+    a {
+        color: white;
+        text-decoration: none;
+        padding: 5px;
+    }
+
+}
+</style>
